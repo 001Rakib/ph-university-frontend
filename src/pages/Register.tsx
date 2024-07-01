94cdb1ce85dd03840d0d90fe56845d774b27791f
001Rakib <rakibrakibrakib354@gmail.com>
2024-07-01 23:42:26 +0600

    created dynamic routes for admin

---
+++ src/pages/Register.tsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <div>
+      <h1>Sign UP page</h1>
+    </div>
+  );
+};
+
+export default Register;
---
+++ src/pages/admin/CreateFaculty.tsx
@@ -0,0 +1,9 @@
+const CreateFaculty = () => {
+  return (
+    <div>
+      <h1>Faculty create page</h1>
+    </div>
+  );
+};
+
+export default CreateFaculty;
