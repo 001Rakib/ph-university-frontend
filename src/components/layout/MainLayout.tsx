90edfdc07e193e1088d4c7b131f2a90228ec35d7
001Rakib <rakibrakibrakib354@gmail.com>
2024-07-03 19:54:05 +0600

    created utility func for dyanmic routes

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,8 @@
 import { Layout, Menu } from "antd";
 import { Outlet } from "react-router-dom";
-import { adminSidebarItems } from "../../routes/admin.routes";
+// import { adminSidebarItems } from "../../routes/admin.routes";
+import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
+import { adminPaths } from "../../routes/admin.routes";
 const { Header, Content, Footer, Sider } = Layout;
 
 // const items: MenuProps["items"] = [
@@ -57,7 +59,7 @@ const MainLayout = () => {
           theme="dark"
           mode="inline"
           defaultSelectedKeys={["4"]}
-          items={adminSidebarItems}
+          items={sidebarItemsGenerator(adminPaths, "admin")}
         />
       </Sider>
       <Layout>
