14c63bf3e50dcc667ff05c97f438a49b0b57fa19
001Rakib <rakibrakibrakib354@gmail.com>
2024-07-04 23:41:31 +0600

    implemented set user to local state and set refresh toke to cookie

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,43 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
+
+// type TUserInfo = {
+//   id: string;
+//   password: string;
+// };
+
 const Login = () => {
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm();
+  const [login, { error }] = useLoginMutation();
+
+  const onSubmit = async (data) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">Id:</label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
   );
 };
 
